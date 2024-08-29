1110b9b18e6fde0e38ae5a653d87de8550aa5e3c
Felipe Brandao <brandao154@outlook.com>
2024-08-29 16:51:58 -0300

    WebAPI implementation

---
+++ WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,86 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var category = await _categoryService.GetAllCategories();
+
+            if (category == null)
+            {
+                return NotFound("Category is empty");
+            }
+
+            return Ok(category);
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<CategoryDTO>> GetById(int? id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            if (category == null)
+            {
+                return NotFound("Could not found ID");
+            }
+
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryDTO>> Create([FromBody] CategoryDTO category)
+        {
+            if (category is null)
+            {
+                return BadRequest("There is an error with your request");
+            }
+
+            await _categoryService.Add(category);
+
+            return new CreatedAtRouteResult("GetProduct", new {id = category.Id}, category);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<CategoryDTO>> Update(CategoryDTO category)
+        {
+            if (category is null)
+            {
+                return BadRequest("There is an error with your request");
+            }
+
+            await _categoryService.Update(category);
+
+            return Ok(category);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int? id)
+        {
+            var category = await _categoryService.GetCategoryById(id);
+
+            if (category == null)
+            {
+                return NotFound("Could not found ID");
+            }
+
+            await _categoryService.Delete(category.Id);
+
+            return Ok(category);
+
+        }
+    }
+}
